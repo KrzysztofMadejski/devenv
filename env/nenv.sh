8316704539b4054ede40a75a20e5c051cef790d9
Krzysztof Madejski <krzysztof@madejscy.pl>
2017-06-02 17:26:40 +0200

    nenv, rbenv

--- env/nenv.sh
+++ env/nenv.sh
@@ -1,6 +1,25 @@
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | bash
+#!/bin/bash
+local=0
+while getopts "gh" opt; do
+  case "$opt" in
+  h|\?)
+      show_help
+      exit 0
+      ;;
+  l)  local=1
+      ;;
+  esac
+done
 
-export NVM_DIR="$HOME/.nvm"
+if [ $local ]; then
+	local install curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
+	export NVM_DIR="$HOME/.nvm"
+else
+	curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | sudo NVM_DIR=/usr/local/nvm bash
+	export NVM_DIR="/usr/local/nvm"
+fi
+
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 nvm install node
