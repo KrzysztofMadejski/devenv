1acc590d7a869d17b3d2a57ea3875a955b8e6ae5
Krzysztof Madejski <krzysztof@madejscy.pl>
2019-09-13 20:25:12 +0200

    different path to git ignore

--- set-config.sh
+++ set-config.sh
@@ -4,8 +4,6 @@ repo="$(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # TODO ln
 cp -i $repo/config/.* ~/
-mkdir -p ~/.config/git
-mv ~/.git.ignore ~/.config/git/ignore
 
 echo ". ~/.bashrc.ext" >> ~/.bashrc
 
